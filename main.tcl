d9e6641dd2015def9575ad4591897a9799a8a140
pounceandmiss <pounceandmiss@proton.me>
2026-03-16 19:02:00 +0400

    --help for cli options

--- main.tcl
+++ main.tcl
@@ -1,4 +1,25 @@
 #!/usr/bin/env tclsh9.0
+
+if {"-h" in $argv || "--help" in $argv || "-help" in $argv} {
+    puts "Usage: tacky \[options\]
+
+Options:
+  --debug-dir DIR   Write per-account debug logs to DIR
+  --backend MODE    Backend mode: direct (default), thread, process
+  --transient yes   Work purely in RAM - don't read/write settings/cache
+  -h, --help        Display this help text and exit"
+    exit 0
+}
+
+# Normalize --foo to -foo for snit
+set argv [lmap arg $argv {
+    if {[string match --* $arg]} {
+        string range $arg 1 end
+    } else {
+        set arg
+    }
+}]
+
 package require Tk
 package require snit
 
